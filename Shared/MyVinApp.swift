bdfeb733c21090f46e95fb1413c3ebd64bd2f130
Adam <adam.mabrouki@webwag.com>
2022-03-25 10:07:47 +0100

    Initial Commit

---
+++ Shared/MyVinApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVinApp.swift
+//  Shared
+//
+//  Created by Adam Mabrouki on 25/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyVinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
